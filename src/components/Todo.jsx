811b4703a1cbfa96faf6bd0385f2b1c88324ea0d
baobeee <nguyenbao0654@gmail.com>
2025-09-25 22:58:13 +0700

    Complete add and delete feature

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import todo_icon from '../assets/todo_icon.png'
+import TodoItems from './TodoItems'
 
 const Todo = () => {
+    const [todoList, setTodoList] = useState([])
+  
+  const inputRef = useRef()
+  const add =()=>{
+    const inputText = inputRef.current.value.trim()
+    if(inputText === '') return null
+
+    const newTodo = {
+      id: Date.now(),
+      text: inputText,
+      isComplete: false
+    }
+
+    setTodoList(prev => [...prev, newTodo])
+    inputRef.current.value=''//clear text field after added
+    inputRef.current.focus()//focus after added
+  }
+
+  const deleteTodo =(id)=>{
+    setTodoList(prev=>{
+      return prev.filter((todo)=> todo.id !== id)//giữ lại todo nào khác với id muốn xóa
+    })
+  }
   return (
     <div className='bg-white place-self-center
      w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl'>
@@ -14,12 +38,16 @@ const Todo = () => {
 
         {/* ---------------Input box--------------- */}
         <div className='flex items-center my-7 bg-gray-200 rounded-full'>
-            <input className='bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600' type="text"  placeholder='Add your task' />
-            <button className='border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer'>Add +</button>
+            <input ref={inputRef} className='bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600' type="text"  placeholder='Add your task' />
+            <button onClick={add} className='border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer'>Add +</button>
         </div>
 
         {/* ---------------Todo list--------------- */}
-
+        <div>
+          {todoList.map((item, index)=>{
+            return <TodoItems key={index} text={item.text} id={item.id} isComplete={item.isComplete} deleteTodo={deleteTodo}/>//props: text
+          })}
+        </div>
     </div>
   )
 }
